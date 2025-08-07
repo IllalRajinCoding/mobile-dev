7a2110da0412e0b1dceea906a1e0336d56dc4e8c
IllalRajinCoding <roxyuciha@gmail.com>
2025-08-07 13:53:30 +0700

    commit

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+// 48
+const profile = () => {
+  return (
+    <View>
+      <Text>profile</Text>
+    </View>
+  )
+}
+
+export default profile
+
+const styles = StyleSheet.create({})
